1100b6b0ca36f626fa8d4f272b799712834b34e8
Nick <nickorsk2017@gmail.com>
2021-01-25 14:10:42 +0500

    fix types of action store

--- store/actions/doctorsListActions.ts
+++ store/actions/doctorsListActions.ts
@@ -1,9 +1,10 @@
 import {
   PUSH_DOCTOR_LIST
 } from '../actionTypes';
+import {Doctor} from "@_types/doctor";
 
 
-export const pushDoctorList = (doctorList) => (
+export const pushDoctorList = (doctorList: Array<Doctor>) => (
   {
     type: PUSH_DOCTOR_LIST,
     doctorList
